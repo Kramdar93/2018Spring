4d86284e568880dc8df9ff24b36aca0a338fc825
Mark Elsinger <thesmokingamp@gmail.com>
2018-02-15 10:47:30 -0500

    Tried out express Copied basic routing functions.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+const express = require('express');
+var app = express();
+
+app.use(function(req, res, next){
+    res.write("first anon function.\r\n");
+    next();
+  });
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/hello', function (req, res) {
+    res.write('hello world');
+    res.end();
+  });
+
+app.get('/goodbye', function (req, res) {
+    res.write('goodbye world');
+    res.end();
+  });
+
+app.listen(8080);
