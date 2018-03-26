7314cdfea606bf6f24cc19b92196d15a012f4bb4
Mark Elsinger <thesmokingamp@gmail.com>
2018-03-26 10:47:00 -0400

    Added Rule Enforcement Cleaned up some visuals in Game component Added ability to add quotes to played quotes, and remove it from available.

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Game } from '../models/game';
+import { Game, Quote, User } from '../models/game';
 
 @Component({
   selector: 'app-game',
@@ -10,9 +10,32 @@ export class GameComponent implements OnInit {
 
   model = new Game();
 
+  currentUser = new User();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  //include event so we can get more information if needed.
+  //mouse event since it will be onclick.
+  //todo: refactor into a function in model for data and make this for display
+  SubmitQuote(e:MouseEvent, text:string){
+    e.preventDefault; //stops browser from taking default action eg navigate away.
+
+    if (!this.MyPlayedQuote()) //only do something if we have not played a quote. truthy if one has been found.
+    {
+      this.model.playedQuotes.push(new Quote(text, this.currentUser.name)); //this line could be used in model.
+
+      var index = this.model.myQuotes.indexOf(text); //get index of selected quote in myquotes array
+      if(index >= 0){ //if we got something
+        this.model.myQuotes.splice(index, 1); //remove it
+      }
+    }
+  }
+
+  MyPlayedQuote(): Quote | null { // * | null needed in case strict checking is enabled since we may not find a quote -> null obj.
+    return this.model.playedQuotes.find(x => x.playerName == this.currentUser.name);
   }
 
 }
--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -7,8 +7,9 @@ export class Game {
     picture:string = "";
 
     constructor(){
-        this.players = [new User("alice"), new User("bob"), new User("charles")];
-        this.playedQuotes = [new Quote("tfw you ate the whole pringles can"), new Quote("too many airheads"), new Quote("but seriously y tho")];
+        this.players = [{name:'alice'}, {name:'bob'}, {name:'charlie'}];
+        //this.playedQuotes = [new Quote("tfw you ate the whole pringles can"), new Quote("too many airheads"), new Quote("but seriously y tho")];
+        this.playedQuotes = [{chosen:false,text:'tfw you ate the whole pringles can',playerName:'jerry136'}]
         this.myQuotes = ["here come dat boi", "mrw i have social anxiety", "a e s t h e t i c s"];
         this.picture = "https://pbs.twimg.com/profile_images/867104339151736832/hyT0nhB4_400x400.jpg";
     }
@@ -17,8 +18,8 @@ export class Game {
 export class User{
     name:string;
 
-    constructor(newName:string){
-        this.name = newName;
+    constructor(){
+        this.name = 'mark';
     }
 }
 
@@ -27,7 +28,8 @@ export class Quote{
     text:string;
     playerName:string;
 
-    constructor(newText:string){
+    constructor(newText:string, player:string){
         this.text = newText;
+        this.playerName = player;
     }
 }
