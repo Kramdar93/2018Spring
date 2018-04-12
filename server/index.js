c7041e20625e79ebb09286c8356508c0c990081f
Mark Elsinger <thesmokingamp@gmail.com>
2018-04-12 11:09:39 -0400

    Middleware Madness Added more middleware functions for serving static files from dist Added build-dev command to package.json Additions now make ng serve optional, server/index.js can handle all site functions

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-var app = express();
+const path = require('path');
+const app = express();
 
 const serverName = "localhost";
 const port = 8080;
@@ -8,15 +9,19 @@ const simple = require('./simpleController');
 const game = require('./game/controller');
 
 app
-    .use("/",(req,res,next)=>{
+    .use("/", (req,res,next)=>{
         //middleware to allow passing more data
         res.header("Access-Control-Allow-Origin","*");
         res.header("Access-Control-Allow-Methods","*");
         res.header("Access-Control-Allow-Headers","*");
         next();
     })
+    .use("/", express.static( path.join(__dirname,"../dist/") ))
     .use("/simple",simple)
     .use("/game",game)
+    .use("/", (req,res,next)=>{
+        res.sendFile(path.join(__dirname,"../dist/")); //if we've found nothing else, go to index
+    })
     .listen(port)
 
 console.log("web server running on http://" + serverName + ":" + port +"/simple");
